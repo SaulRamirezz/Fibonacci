6ef8ce46e3d02da7ec65c008519bdf7a6339782e
SaulRamirezz <ldv19c.sramirez@uartesdigitales.edu.mx>
2020-05-26 20:36:23 -0500

    Agregar archivos de proyecto.

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n = 12, n1 = 0, n2 = 1, n3 = 0;
+
+	cout << "1 ";
+	for (int i = 1; i < n; i++) {
+		n3 = n1 + n2;
+		cout << n3 << " ";
+		n1 = n2;
+		n2 = n3;
+	}
+	
+	return 0;
+}
